867592ed95097ff398205a92669cf16409b509b1
sueyang laryang <ds59013447@gmail.com>
2022-12-25 17:58:32 +0700

    sue frist commit

--- src/components/Sekong/sekongplaces.js
+++ src/components/Sekong/sekongplaces.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import Package1 from '../../img/packageimage01.jpg'
-import Package2 from '../../img/packageimage02.jpg'
-import Package3 from '../../img/packageimage03.jpg'
-import Package4 from '../../img/packageimage04.jpg'
+import Package1 from '../../img/sekong1.jpg'
+import Package2 from '../../img/sekong2.jpg'
+import Package3 from '../../img/sekong3.jpg'
+import Package4 from '../../img/sekong4.jpg'
 import Package5 from '../../img/packageimage05.jpg'
 import Package6 from '../../img/packageimage06.jpg'
 
@@ -13,7 +13,7 @@ export default function sekongplaces() {
         <div className="container-fluid py-5">
         <div className="container pt-5 pb-3">
             <div className="text-center mb-3 pb-3">
-                <h6 className="text-primary text-uppercase" style={{letterSpacing: "5px"}}>ສະເໝີ</h6>
+                <h6 className="text-primary text-uppercase" style={{letterSpacing: "3px"}}>ນຳສະເໝີແຂວງເຊກອງ</h6>
                 <h1>ແຫ່ງທ່ອງທ່ຽວທີ່ຍອດນິຍົມ</h1>
             </div>
             <div className="row">
@@ -22,16 +22,13 @@ export default function sekongplaces() {
                         <img className="img-fluid" src={Package1} alt=""/>
                         <div className="p-4">
                             <div className="d-flex justify-content-between mb-3">
-                                <small className="m-0"><i className="fa fa-map-marker-alt text-primary mr-2"></i>sekong</small>
+                                <small className="m-0"><i className="fa fa-map-marker-alt text-primary mr-2"></i>ເມືອງລະມາມ</small>
                                 <small className="m-0"><i className="fa fa-calendar-alt text-primary mr-2"></i>3 days</small>
                                 <small className="m-0"><i className="fa fa-user text-primary mr-2"></i>2 Person</small>
                             </div>
-                            <a className="h5 text-decoration-none" href="/#">Discover amazing places of the world with us</a>
+                            <a className="h5 text-decoration-none" href="/#">ແຫຼ່ງທ່ອງທ່ຽວທຳມະຊາດ ຕາດແຝກເປັນແຫຼ່ງທ່ອງທ່ຽວທໍາມະຊາດອີກແຫ່ງ ໜຶ່ງທີ່ມີຄວາມສວຍສົດງົດງາມ ແລະ ອຸດົມສົມບູນໄປດ້ວຍປ່າໄມ້ທຳມະຊາດ, ອາກາດປອດໂປ່ງ ແລະ ມີນ້ຳໄຫຼຜ່ານ ຕະຫຼອດລະດູການ ເຊິ່ງເໝາະແກ່ການທ່ອງທ່ຽວ ແລະ ພັກຜ່ອນຢ່ອນກາຍ. </a>
                             <div className="border-top mt-4 pt-4">
-                                <div className="d-flex justify-content-between">
-                                    <h6 className="m-0"><i className="fa fa-star text-primary mr-2"></i>4.5 <small>(250)</small></h6>
-                                    <h5 className="m-0">$350</h5>
-                                </div>
+                            
                             </div>
                         </div>
                     </div>
@@ -41,16 +38,13 @@ export default function sekongplaces() {
                         <img className="img-fluid" src={Package2} alt=""/>
                         <div className="p-4">
                             <div className="d-flex justify-content-between mb-3">
-                                <small className="m-0"><i className="fa fa-map-marker-alt text-primary mr-2"></i>Laos</small>
+                                <small className="m-0"><i className="fa fa-map-marker-alt text-primary mr-2"></i>ເມືອງທ່າແຕງ</small>
                                 <small className="m-0"><i className="fa fa-calendar-alt text-primary mr-2"></i>3 days</small>
                                 <small className="m-0"><i className="fa fa-user text-primary mr-2"></i>2 Person</small>
                             </div>
-                            <a className="h5 text-decoration-none" href="/#">Discover amazing places of the world with us</a>
+                            <a className="h5 text-decoration-none" href="/#">ຕາດປະອໍ ແມ່ນຕາດຂອງຫ້ວຍນ້ຳໃສເປັນເຂດສະຫງວນ ເພື່ອການທ່ອງທ່ຽວ, ທາງດ້ານທຳມະຊາດຍັງຄົງເດີມ, ອາກາດກໍ່ປອດໂປ່ງດີ ເໝາະແກ່ການທ່ອງທ່ຽວ ແລະ ພັກຜ່ອນ.</a>
                             <div className="border-top mt-4 pt-4">
-                                <div className="d-flex justify-content-between">
-                                    <h6 className="m-0"><i className="fa fa-star text-primary mr-2"></i>4.5 <small>(250)</small></h6>
-                                    <h5 className="m-0">$350</h5>
-                                </div>
+                                
                             </div>
                         </div>
                     </div>
@@ -60,16 +54,13 @@ export default function sekongplaces() {
                         <img className="img-fluid" src={Package3} alt=""/>
                         <div className="p-4">
                             <div className="d-flex justify-content-between mb-3">
-                                <small className="m-0"><i className="fa fa-map-marker-alt text-primary mr-2"></i>Laos</small>
+                                <small className="m-0"><i className="fa fa-map-marker-alt text-primary mr-2"></i>ເມືອງລະມາມ</small>
                                 <small className="m-0"><i className="fa fa-calendar-alt text-primary mr-2"></i>3 days</small>
                                 <small className="m-0"><i className="fa fa-user text-primary mr-2"></i>2 Person</small>
                             </div>
-                            <a className="h5 text-decoration-none" href="/#">Discover amazing places of the world with us</a>
+                            <a className="h5 text-decoration-none" href="/#">ຕາດຫົວຄົນເປັນແຫຼ່ງທ່ອງທ່ຽວທຳມະຊາດທີ່ສວຍງາມທາງດ້ານຊັບພະຍາກອນທຳມາຊາດມີສະພາບແວດລ້ອມທີ່ສົມບູນແບບທີ່ສຸດອີກດ້ວຍ</a>
                             <div className="border-top mt-4 pt-4">
-                                <div className="d-flex justify-content-between">
-                                    <h6 className="m-0"><i className="fa fa-star text-primary mr-2"></i>4.5 <small>(250)</small></h6>
-                                    <h5 className="m-0">$350</h5>
-                                </div>
+                                
                             </div>
                         </div>
                     </div>
@@ -79,11 +70,11 @@ export default function sekongplaces() {
                         <img className="img-fluid" src={Package4} alt=""/>
                         <div className="p-4">
                             <div className="d-flex justify-content-between mb-3">
-                                <small className="m-0"><i className="fa fa-map-marker-alt text-primary mr-2"></i>Laos</small>
+                                <small className="m-0"><i className="fa fa-map-marker-alt text-primary mr-2"></i>ສວນຫຼັກເມືອງ</small>
                                 <small className="m-0"><i className="fa fa-calendar-alt text-primary mr-2"></i>3 days</small>
                                 <small className="m-0"><i className="fa fa-user text-primary mr-2"></i>2 Person</small>
                             </div>
-                            <a className="h5 text-decoration-none" href="/#">Discover amazing places of the world with us</a>
+                            <a className="h5 text-decoration-none" href="/#">ສະຖານທີ່ທ່ອງທ່ຽວທາງວັດທະນະທຳທີ້ຊີ້ແຈງເຖິງຄວາມອຸດົມຮັ່ງມີ​ ດ້ວຍເ​ງິນ​, ຄຳ​, ເພັດນິນຈິນດາ​ຕ່າງໆ.</a>
                             <div className="border-top mt-4 pt-4">
                                 <div className="d-flex justify-content-between">
                                     <h6 className="m-0"><i className="fa fa-star text-primary mr-2"></i>4.5 <small>(250)</small></h6>
@@ -104,10 +95,7 @@ export default function sekongplaces() {
                             </div>
                             <a className="h5 text-decoration-none" href="/#">Discover amazing places of the world with us</a>
                             <div className="border-top mt-4 pt-4">
-                                <div className="d-flex justify-content-between">
-                                    <h6 className="m-0"><i className="fa fa-star text-primary mr-2"></i>4.5 <small>(250)</small></h6>
-                                    <h5 className="m-0">$350</h5>
-                                </div>
+                                
                             </div>
                         </div>
                     </div>
@@ -123,10 +111,7 @@ export default function sekongplaces() {
                             </div>
                             <a className="h5 text-decoration-none" href="/#">Discover amazing places of the world with us</a>
                             <div className="border-top mt-4 pt-4">
-                                <div className="d-flex justify-content-between">
-                                    <h6 className="m-0"><i className="fa fa-star text-primary mr-2"></i>4.5 <small>(250)</small></h6>
-                                    <h5 className="m-0">$350</h5>
-                                </div>
+                                
                             </div>
                         </div>
                     </div>
--- src/components/attapeu/attapeuplaces.js
+++ src/components/attapeu/attapeuplaces.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import Package1 from '../../img/packageimage01.jpg'
-import Package2 from '../../img/packageimage02.jpg'
-import Package3 from '../../img/packageimage03.jpg'
+import Package1 from '../../img/attapeu11.jpg'
+import Package2 from '../../img/attapeu22.jpg'
+import Package3 from '../../img/attapeu33.jpg'
 import Package4 from '../../img/packageimage04.jpg'
 import Package5 from '../../img/packageimage05.jpg'
 import Package6 from '../../img/packageimage06.jpg'
@@ -22,16 +22,13 @@ export default function attapeuplaces() {
                         <img className="img-fluid" src={Package1} alt=""/>
                         <div className="p-4">
                             <div className="d-flex justify-content-between mb-3">
-                                <small className="m-0"><i className="fa fa-map-marker-alt text-primary mr-2"></i>attapeu</small>
+                                <small className="m-0"><i className="fa fa-map-marker-alt text-primary mr-2"></i>ເມືອງສະໜາມໄຊ</small>
                                 <small className="m-0"><i className="fa fa-calendar-alt text-primary mr-2"></i>3 days</small>
                                 <small className="m-0"><i className="fa fa-user text-primary mr-2"></i>2 Person</small>
                             </div>
-                            <a className="h5 text-decoration-none" href="/#">Discover amazing places of the world with us</a>
+                            <a className="h5 text-decoration-none" href="/#">ເປັນແຫຼ່ງທ່ອງທ່ຽວນ້ຳຕົກຕາດທຳມະຊາດທີ່ສວຍງາມທີສຸດເໝາະແກ່ການຍ່າງຊົມວິວແລະກິນເຂົ້າປ່າ.</a>
                             <div className="border-top mt-4 pt-4">
-                                <div className="d-flex justify-content-between">
-                                    <h6 className="m-0"><i className="fa fa-star text-primary mr-2"></i>4.5 <small>(250)</small></h6>
-                                    <h5 className="m-0">$350</h5>
-                                </div>
+                                
                             </div>
                         </div>
                     </div>
@@ -41,16 +38,13 @@ export default function attapeuplaces() {
                         <img className="img-fluid" src={Package2} alt=""/>
                         <div className="p-4">
                             <div className="d-flex justify-content-between mb-3">
-                                <small className="m-0"><i className="fa fa-map-marker-alt text-primary mr-2"></i>Laos</small>
+                                <small className="m-0"><i className="fa fa-map-marker-alt text-primary mr-2"></i>ເມືອງຊານໄຊ</small>
                                 <small className="m-0"><i className="fa fa-calendar-alt text-primary mr-2"></i>3 days</small>
                                 <small className="m-0"><i className="fa fa-user text-primary mr-2"></i>2 Person</small>
                             </div>
-                            <a className="h5 text-decoration-none" href="/#">Discover amazing places of the world with us</a>
+                            <a className="h5 text-decoration-none" href="/#">ໜອງຟ້າ ເປັນໜອງນ້ຳທີ່ໃຫຍ່, ມີແປວທາງນ້ຳໄຫຼ ປ່ອງລົງໃສ່ນ້ຳເຊກະໝານ, ນ້ຳໜອງແຫ່ງນີ້ເປັນສີຂຽວ ຄືນ້ຳທະເລ ແລະ ໃສສະອາດ ສາມາດດື່ມໄດ້ ຄືນ້ຳດື່ມບໍລິສຸດ.</a>
                             <div className="border-top mt-4 pt-4">
-                                <div className="d-flex justify-content-between">
-                                    <h6 className="m-0"><i className="fa fa-star text-primary mr-2"></i>4.5 <small>(250)</small></h6>
-                                    <h5 className="m-0">$350</h5>
-                                </div>
+                               
                             </div>
                         </div>
                     </div>
@@ -60,16 +54,13 @@ export default function attapeuplaces() {
                         <img className="img-fluid" src={Package3} alt=""/>
                         <div className="p-4">
                             <div className="d-flex justify-content-between mb-3">
-                                <small className="m-0"><i className="fa fa-map-marker-alt text-primary mr-2"></i>Laos</small>
+                                <small className="m-0"><i className="fa fa-map-marker-alt text-primary mr-2"></i>ເມືອງຊານໄຊ</small>
                                 <small className="m-0"><i className="fa fa-calendar-alt text-primary mr-2"></i>3 days</small>
                                 <small className="m-0"><i className="fa fa-user text-primary mr-2"></i>2 Person</small>
                             </div>
-                            <a className="h5 text-decoration-none" href="/#">Discover amazing places of the world with us</a>
+                            <a className="h5 text-decoration-none" href="/#">ລູກສອນໄຟໂຊຫວຽດເປັນແລ່ງທ່ອງທ່ຽວທາງປະຫວັດສາດເປັນຮ່ອງຮອຍອະນຸສອນສົງຄາມ ໃນສະໄໝການປະຕິວັດປົດປ່ອຍພາກໃຕ້ຂອງລາວ.</a>
                             <div className="border-top mt-4 pt-4">
-                                <div className="d-flex justify-content-between">
-                                    <h6 className="m-0"><i className="fa fa-star text-primary mr-2"></i>4.5 <small>(250)</small></h6>
-                                    <h5 className="m-0">$350</h5>
-                                </div>
+                                
                             </div>
                         </div>
                     </div>
